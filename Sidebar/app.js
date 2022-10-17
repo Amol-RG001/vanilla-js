fbbe04dd829e3d8add1f5d75757545ef5baf3dac
Amol Gahane <amolg10@birlasoft.com>
2022-10-17 16:34:32 +0530

    Add project files.

---
+++ Sidebar/app.js
@@ -0,0 +1,20 @@
+// JavaScript source code
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", function () {
+    //if (sidebar.classList.contains("show-sidebar")) {
+    //    sidebar.classList.remove("show-sidebar");
+    //}
+    //else {
+    //    sidebar.classList.add("show-sidebar");
+    //}
+
+    //same of above code  in shortcut 
+    sidebar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", function () {
+    sidebar.classList.remove("show-sidebar");
+});
